7e1b9e684214ba226190946b0d776416f69e9190
hejk <hejk@lightictech.ic>
2026-04-21 11:11:22 +0800

    add vtk

--- .vscode/ctest_with_vsenv.cmd
+++ .vscode/ctest_with_vsenv.cmd
@@ -1,5 +1,9 @@
 @echo off
 setlocal
+set "http_proxy=http://127.0.0.1:7897"
+set "https_proxy=http://127.0.0.1:7897"
+set "HTTP_PROXY=http://127.0.0.1:7897"
+set "HTTPS_PROXY=http://127.0.0.1:7897"
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 if errorlevel 1 exit /b %errorlevel%
 ctest %*
--- inc/mainwindow.h
+++ inc/mainwindow.h
@@ -3,11 +3,25 @@
 
 #include <QMainWindow>
 
+#include <memory>
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+class QTimer;
+class QWidget;
+
+namespace open3d {
+namespace geometry {
+class PointCloud;
+}
+namespace visualization {
+class Visualizer;
+}
+}
 
 class MainWindow : public QMainWindow
 {
@@ -18,6 +32,13 @@ public:
     ~MainWindow() override;
 
 private:
+    void InitOpen3DWidget();
+    std::shared_ptr<open3d::geometry::PointCloud> CreateDemoPointCloud() const;
+
     Ui::MainWindow *ui;
+    QWidget *open3dHostWidget_ = nullptr;
+    QWidget *open3dContainerWidget_ = nullptr;
+    QTimer *renderTimer_ = nullptr;
+    std::unique_ptr<open3d::visualization::Visualizer> visualizer_;
 };
 #endif // MAINWINDOW_H
